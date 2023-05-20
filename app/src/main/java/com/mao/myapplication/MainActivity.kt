f52170b72ecc1d121ea0d654cf418d94b11ad236
Mauricio Sartori <omauricioguerrero@gmail.com>
2023-05-20 13:22:19 -0700

    feat: add basic project structure

---
+++ app/src/main/java/com/mao/myapplication/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mao.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "ESCOM is working",Toast.LENGTH_LONG).show()
+    }
+}
