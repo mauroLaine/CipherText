2dfabedcca944ce9231fd46df42d4d62fc670bbd
Mauricio Sartori <omauricioguerrero@gmail.com>
2023-05-21 22:47:49 -0700

    chore: add navigation to camera

--- app/src/main/java/com/mao/myapplication/MainActivity.kt
+++ app/src/main/java/com/mao/myapplication/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.mao.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import com.mao.myapplication.databinding.ActivityMainBinding
@@ -16,10 +16,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         // When using Latin script library
         val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-        
+
         binding.btnTakePicture.setOnClickListener {
-            Toast.makeText(this, "Go to camera activity", Toast.LENGTH_SHORT).show()
-            // Add your desired functionality here when the button is clicked
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
         }
     }
 }
